4041b2d9d782966de79e643b920df67d19bc64ec
Глеб Боушев <core@4c74356b41.com>
2016-02-26 20:51:53 +0300

    Moved script to separate file

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+﻿blurredImages = $( '.blur-me' );
+blurredImages.on( "mouseenter", function () {
+    $(this).addClass("blurred");
+    $(this).children().addClass("onHover");
+    $(this).children().removeClass("hidden");
+})
+blurredImages.on( "mouseleave", function () {
+    $(this).removeClass("blurred");
+    $(this).children().removeClass("onHover");
+    $(this).children().addClass("hidden");
+})
