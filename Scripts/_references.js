02a53c76813ae31f6521bc9aa06e20310ecd52d7
Глеб Боушев <core@4c74356b41.com>
2016-02-28 15:49:06 +0300

    played a little bit with jQuery, testing

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,2 +1,3 @@
 ﻿/// <reference path="jquery-2.2.0.min.js" />
 /// <reference path="bootstrap.min.js" />
+/// <reference path="bootstrap-switch.min.js" />
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,6 @@
-﻿blurredImages = $( '.blur-me' );
+﻿/*
+
+blurredImages = $( '.blur-me' );
 blurredImages.on( "mouseenter", function () {
     $(this).addClass("blurred");
     $(this).children().addClass("onHover");
@@ -9,3 +11,28 @@ blurredImages.on( "mouseleave", function () {
     $(this).children().removeClass("onHover");
     $(this).children().addClass("hidden");
 })
+
+$( "#testid" ).on("click", function () {
+    console.log($(this).text());
+    console.log($(this));
+    $(this).css("background-image", 'url(Img/twilight_sparkle.jpg)');
+})
+
+*/
+
+$("[name='my-checkbox']").bootstrapSwitch();
+$("[name='my-checkbox']").bootstrapSwitch('onText', 'Goodies');
+$("[name='my-checkbox']").bootstrapSwitch('offText', 'Baddies');
+$("[name='my-checkbox']").bootstrapSwitch('onColor', 'success');
+$("[name='my-checkbox']").bootstrapSwitch('offColor', 'danger');
+$("[name='my-checkbox']").parent().parent().css({ "position": "absolute", "right": "100px" });
+
+$('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function (event, state) {
+    if (state) {
+        $("[id='rarity']").addClass("rarity");
+        $("[id='rarity']").removeClass("chrysalis");
+    } else {
+        $("[id='rarity']").removeClass("rarity");
+        $("[id='rarity']").addClass("chrysalis");
+    }
+});
