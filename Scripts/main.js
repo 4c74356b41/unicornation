135de266269da3f2ad7e9a96af97bc2ed0d0f1e2
Глеб Боушев <core@4c74356b41.com>
2016-02-28 16:51:48 +0300

    Removed some unnecessary code, Minor style adjustments

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,24 +1,15 @@
-﻿/*
-
-blurredImages = $( '.blur-me' );
+﻿blurredImages = $( '.blur-me' );
 blurredImages.on( "mouseenter", function () {
     $(this).addClass("blurred");
-    $(this).children().addClass("onHover");
-    $(this).children().removeClass("hidden");
+    $(this).next().addClass("onHover");
+    $(this).next().removeClass("hidden");
 })
 blurredImages.on( "mouseleave", function () {
     $(this).removeClass("blurred");
-    $(this).children().removeClass("onHover");
-    $(this).children().addClass("hidden");
+    $(this).next().removeClass("onHover");
+    $(this).next().addClass("hidden");
 })
 
-$( "#testid" ).on("click", function () {
-    console.log($(this).text());
-    console.log($(this));
-    $(this).css("background-image", 'url(Img/twilight_sparkle.jpg)');
-})
-
-*/
 
 $("[name='my-checkbox']").bootstrapSwitch();
 $("[name='my-checkbox']").bootstrapSwitch('onText', 'Goodies');
